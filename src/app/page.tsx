594aee231a70b06a4c7414e45c4e975f7a193535
annalhq <155549764+annalhq@users.noreply.github.com>
2025-03-21 18:49:16 +0530

    feat: add parallax floats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import { Preview } from "@/components/preview";
 import { HeroGeometric } from "@/components/ui/shape-landing-hero";
 
 export default function Home() {
   return (
-    <HeroGeometric
-    badge="Upcoming event on 29th March"
-    title1="Innovate, Code"
-    title2="with GDG RBU"
-    />
+    <div className="flex flex-col">
+      <section className="w-full relative">
+        <HeroGeometric
+          badge="Upcoming event on 29th March"
+          title1="Innovate, Code"
+          title2="with GDG RBU"
+        />
+      </section>
+
+      <section className="w-full relative">
+        <Preview />
+      </section>
+    </div>
   );
 }
